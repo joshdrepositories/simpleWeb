6ef7323cea8be8382e4a49b10372f3f57beb5762
Ethan M. Lee <ethan@mlee.xyz>
2024-07-11 15:04:46 -0400

    comments and other shit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+
+# source secret variables
 source .env
+# build docker image image
 docker build -t $IMAGE_NAME:latest .
-#docker save $IMAGE_NAME:latest | ssh -C $VPS docker load
+# send build image to server over ssh
+docker save $IMAGE_NAME:latest | ssh -C $VPS docker load
+# make directory where docker-compose.yml will live
 ssh $VPS "mkdir $REMOTE_DIR"
-rsync -avz --mkpath docker-compose.yml $VPS:$REMOTE_DIR/docker-compose.yml
+# copy docker-compose.yml to vps
+rsync -avz docker-compose.yml $VPS:$REMOTE_DIR/docker-compose.yml
+# bring that bitch up
+ssh $VPS "cd $REMOTE_DIR; docker compose up -d"
