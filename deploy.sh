de418b12d8e235d03fc97941339f825a2f769a92
Ethan M. Lee <ethan@mlee.xyz>
2024-07-15 13:49:50 -0400

    deploy script posix compliant

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # source secret variables
-source .env
+. ./.env
 # build docker image image
 docker build -t $IMAGE_NAME:latest .
 # send build image to server over ssh
 docker save $IMAGE_NAME:latest | ssh -C $VPS docker load
 # make directory where docker-compose.yml will live
-ssh $VPS "mkdir $REMOTE_DIR"
+ssh $VPS "mkdir $REMOTE_DIR/$IMAGE_NAME"
 # copy docker-compose.yml to vps
-rsync -avz docker-compose.yml $VPS:$REMOTE_DIR/docker-compose.yml
+rsync -avz docker-compose.yml $VPS:$REMOTE_DIR/$IMAGE_NAME/docker-compose.yml
 # bring that bitch up
-ssh $VPS "cd $REMOTE_DIR; docker compose up -d"
+ssh $VPS "cd $REMOTE_DIR/$IMAGE_NAME; docker compose up -d"
